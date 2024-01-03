a8e9ee77030dbb1caa41af3b2825cbea21aaccaf
tylersmith <tyjsmith1@gmail.com>
2024-01-02 18:18:01 -0600

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
 def write_file(file_name, file_content):
-    pass
+    file_name = str(file_name) + '.txt'
+
+    with open(file_name, 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_name = str(file_name) +'.txt'
+
+    with open(file_name, 'a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_name = str(file_name) + '.txt'
+
+    with open(file_name, 'r') as file:
+        return file.read()
